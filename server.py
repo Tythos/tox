e4240e50d2795fbab990950e40478a44225de354
Brian Kirkpatrick <code@tythos.net>
2015-12-26 20:59:50 -0800

    Implementation for Passenger WSGI environment

--- server.py
+++ server.py
@@ -5,22 +5,28 @@ hostIp = "127.0.0.1"
 portNumber = 1337
 
 class ImpulseServer(object):
+	def get_path(self, data_path):
+		return os.path.dirname(os.path.realpath(__file__)) + os.sep + data_path
+
 	@cherrypy.expose
 	def index(self):
-		with open("main.html", "r") as f:
+		main_path = self.get_path('main.html')
+		with open(main_path, "r") as f:
 			return f.read()
 		
-if __name__ == "__main__":
-	conf = {
-		'global': {
-			'server.socket_host': hostIp,
-			'server.socket_port': portNumber,
-		},
-		'/': {
-			'tools.sessions.on': True,
-			'tools.staticdir.root': os.path.abspath(os.getcwd()),
-			'tools.staticdir.on': True,
-			'tools.staticdir.dir': ".",
-		}
+conf = {
+	'global': {
+		'server.socket_host': hostIp,
+		'server.socket_port': portNumber,
+	},
+	'/': {
+		'tools.sessions.on': True,
+		'tools.staticdir.root': os.path.abspath(os.getcwd()),
+		'tools.staticdir.on': True,
+		'tools.staticdir.dir': ".",
 	}
+}
+
+if __name__ == '__main__':
 	cherrypy.quickstart(ImpulseServer(), '/', conf)
+
